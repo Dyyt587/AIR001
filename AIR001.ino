1fa9d53634884bf626f9e8ef6511e33c4875d932
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2023-08-09 15:18:02 +0800

    Initial commit

---
+++ AIR001.ino
@@ -0,0 +1,50 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-08-09 13:35:21
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-08-09 15:12:42
+ * @FilePath: \AIR001\AIR001.ino
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 实现ws2812
+ * 实现iic通讯
+ * 实现电机转动
+ * 实现电机位置闭环
+ * 实现通讯协议
+ * 实现动态调参
+ *
+
+
+
+
+
+*/
+
+
+#include "Wire.h"
+
+
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+
+void loop()
+{
+
+
+}
---
+++ arduino/build/sketch/AIR001.ino.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\Administrator\\Desktop\\AIR001\\AIR001.ino"
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-08-09 13:35:21
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-08-09 14:47:19
+ * @FilePath: \AIR001\AIR001.ino
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 实现ws2812
+ * 实现iic通讯
+ * 实现电机转动
+ * 实现电机位置闭环
+ * 实现通讯协议
+ * 实现动态调参
+ *
+
+
+
+
+
+*/
+
+
+
+
+
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+
+void loop()
+{
+
+
+}

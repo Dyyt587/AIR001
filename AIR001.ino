1e4bd10529b2528a478d85f77fee584320da8b2b
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2023-08-09 15:47:25 +0800

    m
    m

--- AIR001.ino
+++ AIR001.ino
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-08-09 13:35:21
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2023-08-09 15:12:42
+ * @LastEditTime: 2023-08-09 15:44:07
  * @FilePath: \AIR001\AIR001.ino
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -40,11 +40,13 @@
 void setup()
 {
     Serial.begin(115200);
+    Wire.begin();
 }
-
 
 void loop()
 {
-
+    Wire.beginTransmission(8);
+    Wire.write('8');
+    Wire.endTransmission();
 
 }
